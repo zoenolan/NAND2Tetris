fe8dfbe555d703b9f8f9f14fda54fb78587d5da6
Zoe Nolan <github@zoenolan.org>
2016-12-11 22:10:38 +0000

    Project 4 submission

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -6,9 +6,135 @@
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
-// the screen should remain fully black as long as the key is pressed. 
+// the screen should remain fully black as long as the key is pressed.
 // When no key is pressed, the program clears the screen, i.e. writes
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// Set the last keyboard input
+@R0
+M=-1
+
+(KEYLOOP)
+
+// Read the keyboard
+@KBD
+D=M
+@R1
+M=D
+
+// Compare with the last value
+@R0
+D=M
+@R1
+D=D-M
+
+@CHANGE
+D;JNE // If (R1 != R0) goto CHANGE
+
+@KEYLOOP
+0;JMP  // Goto KEYLOOP
+
+(CHANGE)
+// Save the new values
+@R1
+D=M
+@R0
+M=D
+
+// Fill the screen
+@FILLSCREEN
+0;JMP  // Goto FILLSCREEN
+
+(FILLSCREENRETURN)
+@KEYLOOP
+0;JMP  // Goto KEYLOOP
+
+(FILLSCREEN)
+
+@8192
+D=A
+@R2
+M=D
+
+@SCREEN
+D=A
+@R3
+M=D
+
+(FILLWORD)
+
+// Fill word
+
+@R0
+D=M
+@BLACK
+D;JNE // If (R0 != 0) goto BLACK
+
+// White
+@R3
+A=M
+M=0
+
+@ENDFILL
+0;JMP  // Goto ENDFILL
+
+// Black
+(BLACK)
+@R3
+A=M
+M=-1
+
+(ENDFILL)
+
+// Move to the next word
+@R3
+D=M
+@1
+D=D+A
+@R3
+M=D
+
+@R2
+M=M-1
+
+@R2
+D=M
+
+@FILLWORD
+D;JGE // If (R2  0) goto FILLWORD
+
+@FILLSCREENRETURN
+0;JMP  // Goto FILLSCREENRETURN
+
+@R0
+D=M
+@0
+D=D-M
+@BLACK
+D;JNE // If (R1 != R0) goto CHANGE
+
+// Fill word
+@17
+A=M
+M=0
+
+@ENDFILL
+0;JMP  // Goto ENDFILL
+
+
+
+
+
+// Move to the next line
+@17
+D=M
+@2
+D=D+A
+@17
+M=D
+
+@16
+MD=M-1
+@10
+D,JGT
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -6,4 +6,35 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Zero the sum
+@R2
+M=0
+
+// Set the counter
+@R3
+M=1
+
+(LOOP)
+@R3
+D=M
+
+@R0
+D=D-M
+@END
+D;JGT // If (R3-R0) > 0 goto END
+
+@R1
+D=M // D = R1
+@R2
+M=D+M // R2 = R2 + R1
+
+@R3  // R3 = R3 + 1
+M=M+1
+
+@LOOP
+0;JMP  // Goto LOOP
+
+// End of program
+(END)
+  @END
+  0;JMP // Infinite loop
